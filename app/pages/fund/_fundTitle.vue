af64bbd97e49f92ecff1a6ab898e40b2db2e6e89
Brostafa <mostafahefny2010@gmail.com>
2020-11-22 23:19:25 +0200

    feat: add period price diff

--- app/pages/fund/_fundTitle.vue
+++ app/pages/fund/_fundTitle.vue
@@ -32,6 +32,28 @@
       >All</v-chip>
     </div>
 		<Chart :data="barChartData" :options="barChartOptions" :height="150" />
+
+      <v-container>
+        <v-row class="text-center">
+          <v-col md="4">
+            Current: {{current}}
+            <span
+              v-if="!isNaN(priceDiff)"
+              style="font-size: 14px;"
+              :class="priceDiff < 0 ? 'red--text' : 'green--text'"
+            >
+              <v-icon size="16" class="mb-1" color="error" v-if="priceDiff < 0">mdi-arrow-down-thick</v-icon>
+              <v-icon size="16" class="mb-1" color="success" v-else>mdi-arrow-up-thick</v-icon>
+              {{priceDiff}}
+              ({{percDiff}}%)
+            </span>
+          </v-col>
+          <v-col md="4">High: {{high}}</v-col>
+          <v-col md="4">Low: {{low}}</v-col>
+        </v-row>
+      </v-container>
+    <section class="mt-2" style="font-size: 17px; font-weight: 500px">
+    </section>
 	</div>
 </template>
 
@@ -57,6 +79,11 @@ export default {
       barChartOptions: {
         responsive: true,
       },
+      high: 0,
+      low: 0,
+      current: 0,
+      priceDiff: 0,
+      percDiff: 0
     }
   },
   created () {
@@ -102,7 +129,18 @@ export default {
       const timeDiff = Date.now() - periodInMs
       const filteredFunds = this.funds.filter(fund => fund.timestamp > timeDiff)
       const prices = filteredFunds.map(fund => fund.price)
-      const isPositive = prices[0] <= prices.slice(-1)[0]
+      const currentPrice = prices.slice(-1)[0]
+      const isPositive = prices[0] <= currentPrice
+      // Get high, low and current
+      const sortedPrices = [...prices].sort()
+      this.high = sortedPrices.slice(-1)[0]
+      this.low = sortedPrices[0]
+      this.current = currentPrice
+
+      const firstPrice = prices[0]
+      this.priceDiff = Math.round((currentPrice - firstPrice) * 100) / 100
+      this.percDiff = Math.round((currentPrice / firstPrice - 1) * 100 * 100) / 100
+      
       this.barChartData.labels = filteredFunds.map(fund => fund.date)
       this.barChartData.datasets[0] = {
         label: 'Net Asset',
